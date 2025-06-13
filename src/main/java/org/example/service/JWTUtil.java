a19c298a1396d26c4356c73873509298d0a5f891
LugrenLaxx <lugrenl@gmail.com>
2025-06-13 20:07:16 +0300

    add_jwtutil

--- src/main/java/org/example/service/JWTUtil.java
+++ src/main/java/org/example/service/JWTUtil.java
@@ -1,8 +1,13 @@
 package org.example.service;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 public class JWTUtil {
@@ -13,9 +18,27 @@ public class JWTUtil {
     @Value("${jwt_expiration}")
     private int expiration;
 
-    public String generateToken(String username) {
-        Date expirationDate = new Date(System.currentTimeMillis() + expiration);
+    private static final String USER_NAME = "username";
 
-        return "token";
+    public String generateToken(String username) {
+        Date expirationDate = Date.from(ZonedDateTime.now().plusMinutes(expiration).toInstant());
+
+        return JWT.create()
+                .withSubject("User details")
+                .withExpiresAt(expirationDate)
+                .withClaim(USER_NAME, username)
+                .sign(Algorithm.HMAC256(secret));
+    }
+
+    public String validateTokenAndRetrieveClaim(String token) throws JWTVerificationException {
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret.getBytes())).build();
+
+        DecodedJWT jwt = verifier.verify(token);
+
+        if (jwt.getClaim(USER_NAME).isMissing()) {
+            throw new JWTVerificationException("invalid token");
+        }
+
+        return jwt.getClaim(USER_NAME).asString();
     }
 }
