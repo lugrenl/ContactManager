bad4c2acd1e01ef6bab7d37a111e11a1b88c8ef3
LugrenLaxx <lugrenl@gmail.com>
2025-06-13 20:48:56 +0300

    add_another_security_parts

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -1,16 +1,72 @@
 package org.example.config;
 
+import org.example.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfig  {
+public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JWTFilter jwtFilter;
+
+    @Autowired
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, JWTFilter jwtFilter) {
+        this.userDetailsService = userDetailsService;
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
+                        .anyRequest().hasAnyRole("USER", "ADMIN")
+                        /// поправить ссылки
+                        .requestMatchers("/api/auth/**").permitAll()
+                        //.requestMatchers("/api/users/register").permitAll()
+                        .requestMatchers("/api/users/**").hasRole("ADMIN")
+                        .requestMatchers("/api/contacts/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/hello", true)
+                        .failureUrl("/auth/login?error")
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/auth/login")
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -1,4 +1,80 @@
 package org.example.controller;
 
+import jakarta.validation.Valid;
+import org.example.dto.AuthenticationDto;
+import org.example.dto.UserDto;
+import org.example.model.User;
+import org.example.service.JWTUtil;
+import org.example.service.RegistrationService;
+import org.example.util.UserValidator;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final RegistrationService registrationService;
+    private final UserValidator userValidator;
+    private final JWTUtil jwtUtil;
+    private final ModelMapper modelMapper;
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthController(RegistrationService registrationService, UserValidator userValidator,
+                          JWTUtil jwtUtil, ModelMapper modelMapper, AuthenticationManager authenticationManager) {
+        this.registrationService = registrationService;
+        this.userValidator = userValidator;
+        this.jwtUtil = jwtUtil;
+        this.modelMapper = modelMapper;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/register")
+    public Map<String, String> performRegistration(@RequestBody @Valid UserDto userDTO,
+                                                   BindingResult bindingResult) {
+        User user = convertToUser(userDTO);
+
+        userValidator.validate(user, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return Map.of("message", "Ошибка!");
+        }
+
+        registrationService.register(user);
+
+        String token = jwtUtil.generateToken(user.getName());
+        return Map.of("jwt-token", token);
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> performLogin(@RequestBody AuthenticationDto authenticationDTO) {
+        UsernamePasswordAuthenticationToken authInputToken =
+                new UsernamePasswordAuthenticationToken(authenticationDTO.getUsername(),
+                        authenticationDTO.getPassword());
+
+        try {
+            authenticationManager.authenticate(authInputToken);
+        } catch (BadCredentialsException e) {
+            return Map.of("message", "Incorrect credentials!");
+        }
+
+        String token = jwtUtil.generateToken(authenticationDTO.getUsername());
+        return Map.of("jwt-token", token);
+    }
+
+    public User convertToUser(UserDto userDTO) {
+        return this.modelMapper.map(userDTO, User.class);
+    }
 }
+
